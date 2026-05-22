f843f9dae628caad2112c6d69079e83c2a769984
nhanh <3201595386@qq.com>
2026-05-22 17:48:26 +0800

    fix(贝塞尔曲线): 采样点仅保留二维坐标
    将 GetBezierCurveNodes 返回值显式取 [x, y] 再入队，保证 curveNodes 类型一致。
    [publish]

--- src/views/canvas/BezierCurve/index.ts
+++ src/views/canvas/BezierCurve/index.ts
@@ -15,7 +15,7 @@ export default function BezierCurve(config: Config) {
   const curveNodes: [number, number][] = [];
   for (let i = 0; i <= progress; ) {
     const node = _Math_GetBezierCurveNodes(nodes, i);
-    curveNodes.push(node);
+    curveNodes.push([node[0], node[1]]);
     if (i < progress) {
       i += precision;
       if (i > progress) i = progress;
